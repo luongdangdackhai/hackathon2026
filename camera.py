5b3a2ae69978810a0afcefb33fc74d70b346fa09
sham <luongdangdackhai@gmail.com>
2026-02-18 21:53:36 -0700

    clean up

--- camera.py
+++
@@ -1,6 +0,0 @@
-from picamzero import Camera
-
-def capture():
-    cam = Camera()
-    cam.take_photo("image.jpg") #save the image to your desktop
-    return 0
--- emotion.py
+++
@@ -1,8 +0,0 @@
-import face_recognition_models
-
-image = face_recognition.load_image_file("image.jpg")
-face_locations = face_recognition_model_location(image)
-
-print(face_locations)
-
-# face_locations is now an array listing the co-ordinates of each face!
